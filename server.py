4970a65dc21bfbe6df35fdd61f553965a67b292e
Ronihe <herongrong2011@gmail.com>
2020-06-16 23:07:49 -0700

    step s binding socket

--- server.py
+++ server.py
@@ -14,4 +14,18 @@ def create_socket():
         s = socket.socket()
     except socket.error as error_msg:
         print("socket creation error:" + str(error_msg))
-    
+
+
+# binding socket an listening for connections
+def bind_socket():
+    try:
+        global host
+        global port
+        global s
+
+        print("binding port:" + str(port))
+        s.bind((host, port))
+        s.listen(5)
+    except socket.error as error_msg:
+        print("socket binding error:" + str(error_msg) + "\n" + "retrying")
+        bind_socket()
